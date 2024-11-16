560873e79e97c10f99a73c570bbedff647b0bb82
Максим Олександрович Зенченко <zenchmax@gmail.com>
2024-11-16 23:03:20 +0200

    Добавьте файлы проекта.

---
+++ CostNl_1.net/CostNl_1.1.net/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CostNl_1.net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите свою Фамилию: ");
+        string surname = Console.ReadLine();
+        Console.Clear();
+        Console.Write("Введите свое Имя: ");
+        string name = Console.ReadLine();
+        Console.Clear();
+        Console.Write("Введите свое Отчество: ");
+        string patronymic = Console.ReadLine();
+        Console.Clear();
+
+        Console.Write("Введите свой возраст: ");
+        int age = int.Parse(Console.ReadLine());
+        Console.Clear();
+
+        Console.Write("Введите свой Адрес (улица, дом, квартира): ");
+        string address = Console.ReadLine();
+        Console.Clear();
+
+        Console.Write("Введите свой номер телефона: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.Clear();
+
+        Console.Write("Введите свой рост в см: ");
+        double growth = double.Parse(Console.ReadLine());
+        Console.Clear();
+
+        Console.Write("Введите свой вес в кг: ");
+        double weight = double.Parse(Console.ReadLine());
+        Console.Clear();
+
+        Console.WriteLine("Вывод в консоль полученные данные\n");
+        Console.WriteLine($"ФИО: {surname} {name} {patronymic}");
+        Console.WriteLine($"Возрост: {age}");
+        Console.WriteLine($"Адрес: {address}");
+        Console.WriteLine($"Номер телефона: {number}");
+        Console.WriteLine($"Рост: {growth} см");
+        Console.WriteLine($"Вес: {weight} кг");
+    }
+}
