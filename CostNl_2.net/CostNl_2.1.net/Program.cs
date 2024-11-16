d969c1e6cd24693df6c6749c87c9e326e16d4402
Максим Олександрович Зенченко <zenchmax@gmail.com>
2024-11-16 23:39:01 +0200

    Start №2 costNl 1 velic

---
+++ CostNl_2.net/CostNl_2.1.net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CostNl_2._1.net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
