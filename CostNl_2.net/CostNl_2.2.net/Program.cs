2dc0fb2302613a38ddc9a91d8c1ed45ce41c5e2a
Максим Олександрович Зенченко <zenchmax@gmail.com>
2024-11-18 10:23:07 +0200

    Створив 2.2 і буду робити числа Амстронга

---
+++ CostNl_2.net/CostNl_2.2.net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CostNl_2._2.net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
